7a1f35415066d2f59ffbd5086cf923e4a2a6091f
kashcodernyc <prakash_subba08@hotmail.com>
2021-08-19 21:39:44 -0400

    additional features
    i am working on removing the displayed weather after the user have searched another location

--- weather.js
+++ weather.js
@@ -5,21 +5,41 @@ const title = document.querySelector('.title');
 const cityData = document.querySelector('#cityname');
 const btnClick = document.querySelector('#submit');
 const cityTemp = document.querySelector('.temp');
+const weatherDetails = document.querySelector('.details');
+const changeWeather = document.querySelector('#reset');
 
 
 
 //&units=imperial
+const weatherList = [];
 
-
+const reset = function(){
+   location.reload()
+}
 
 const renderWeather = function(city){
-    const html = `
-    <p class = 'temp' >${city.name}</p>
-    <p class = 'temp' >${city.main.temp} °F</p>
-    <p class = 'temp'>${city.weather[0].description}</p>
+    const condition = city.weather[0]
+    const icon = condition.icon;
+    
+
+    let html = `
+    <div class = 'details'>
+    <p class = 'temp'>${city.name}</p>
+    <p class = 'temp'>${city.main.temp} °F</p>
+    <img id = 'icon' src = 'http://openweathermap.org/img/w/${icon}.png'>
+    <p class = 'temp'>${condition.description}</p>
+    <p class = 'temp'> max: ${city.main.temp_max} </p>   
+    <p class = 'temp'> min: ${city.main.temp_min}</p>
+    </div>
     `
     header.insertAdjacentHTML('afterend', html);
-}
+    cityData.value = '';
+    weatherList.pop();
+    // btnClick.addEventListener('click',reset);
+    } 
+    
+    
+
 
 
 const getWeather = function (e){
@@ -30,15 +50,18 @@ const getWeather = function (e){
     const weatherData = fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`)
     .then(res => res.json())
     .then(res => {
-        console.log(res)
-        renderWeather(res)
+        weatherList.push(res);
+        renderWeather(res);
     })
     .catch(err => console.log(err)) 
 
-    
-  
+
+    // btnClick.addEventListener('click', clearHtml);
+
+
 }
 
 btnClick.addEventListener('click', getWeather);
+changeWeather.addEventListener('click', reset);
 
 
